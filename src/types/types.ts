11fb0d4bc8876e9d1481b9acfde2d05296655331
Gabriel Macedo <gabmacedo33@gmail.com>
2026-04-21 17:02:04 -0300

    feat: adding types to components

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,25 +1,40 @@
 export interface CardComponentProps {
   type: "login" | "register"
-  name?: string
+  firstName?: string
   lastName?: string
   email?: string
   password?: string
   error?: boolean
+  errorMessage?: string
   loading?: boolean
-  setName?: (name: string) => void
+  setFirstName?: (firstName: string) => void
   setLastName?: (lastName: string) => void
   setEmail?: (email: string) => void
   setPassword?: (password: string) => void
   setStep?: (step: number) => void
-  onSubmit?: (e: React.FormEvent) => void
+  handleRegisterSubmit?: (e: React.FormEvent) => void
+  handleLoginSubmit?: (e: React.FormEvent) => void
 }
 
 export interface InputOTPFormProps {
+  email?: string
+  loading?: boolean
+  errorMessage?: string
+  code?: string
   setStep?: (step: number) => void
+  handleOTPSubmit?: (e: React.FormEvent) => void
+  setCode?: (code: string) => void
 }
 
 export interface UserResearchFormProps {
-  setStep?: (step: number) => void
+  tipoVaga?: string
+  quantidadeAplicacoes?: string
+  organizacaoCandidaturas?: string
+  loading?: boolean
+  setTipoVaga?: (tipoVaga: string) => void
+  setQuantidadeAplicacoes?: (quantidadeAplicacoes: string) => void
+  setOrganizacaoCandidaturas?: (organizacaoCandidaturas: string) => void
+  handleFinalSubmit?: (e: React.FormEvent) => void
 }
 
 export interface HeaderComponentProps {
